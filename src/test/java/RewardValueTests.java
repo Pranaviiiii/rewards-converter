8d52c23dd774163eb9975b9fbf48a4535384b75a
Pranaviiiii <124892955+Pranaviiiii@users.noreply.github.com>
2024-10-02 12:14:08 +0100

    Update RewardValueTests.java

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -7,31 +6,32 @@ public class RewardValueTests {
     @Test
     void create_with_cash_value() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(cashValue, rewardValue.getCashValue(), "Cash value should be accurate.");
     }
 
     @Test
     void create_with_miles_value() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(milesValue, rewardValue.getMilesValue(), "Miles value should be accurate.");
     }
 
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
+        RewardValue rewardValue = new RewardValue(cashValue);
         double expectedMiles = cashValue / 0.0035;
-        assertEquals(expectedMiles, rewardValue.getMilesValue());
+        assertEquals(expectedMiles, rewardValue.getMilesValue(), "Conversion from cash to miles should be accurate.");
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
+        RewardValue rewardValue = new RewardValue(milesValue);
         double expectedCash = milesValue * 0.0035;
-        assertEquals(expectedCash, rewardValue.getCashValue());
+        assertEquals(expectedCash, rewardValue.getCashValue(), "Conversion from miles to cash should be accurate.");
     }
 }
+
 
